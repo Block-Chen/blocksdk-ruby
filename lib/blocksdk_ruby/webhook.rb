a243784bb2824ce81218b302c891fb64c61fbe34
blockchen-admin <61440698+blockchen-admin@users.noreply.github.com>
2021-01-18 16:50:25 +0900

    Update webhook.rb

--- lib/blocksdk_ruby/webhook.rb
+++ lib/blocksdk_ruby/webhook.rb
@@ -3,32 +3,32 @@ require_relative 'base'
 class WebHook < Base
 	
 	def create(request={})
-		return request("POST","/hook",{"callback"=>request['callback'],"category"=>request['category'],"address"=>request['address']})
+		return request("POST","/hooks",{"callback_url"=>request['callback_url'],"symbol"=>request['symbol'],"address"=>request['address']})
 	end
 
 	def list(request = {})
 		request["offset"].to_s.empty? ? request['offset'] = 0 : request["offset"]
 		request["limit"].to_s.empty? ? request['limit'] = 10 : request["limit"]			
-		return request("GET","/hook",{"offset"=>request['offset'],"limit"=>request['limit']})
+		return request("GET","/hooks",{"offset"=>request['offset'],"limit"=>request['limit']})
 	end
 		
 	def get(request={})
-		return request("GET","/hook/" +    (request['hook_id']).to_s)
+		return request("GET","/hooks/" +    (request['hook_id']).to_s)
 	end
 
 	def delete(request={})
-		return request("POST","/hook/" +    (request['hook_id']).to_s + "/delete")
+		return request("DELETE","/hooks/" +    (request['hook_id']).to_s)
 	end
 	
-	def listResponse(request = {})
+	def getResponses(request = {})
 		request["offset"].to_s.empty? ? request['offset'] = 0 : request["offset"]
 		request["limit"].to_s.empty? ? request['limit'] = 10 : request["limit"]
-		return request("GET","/hook/response",{"offset"=>request['offset'],"limit"=>request['limit']})			
+		return request("GET","/hooks/responses",{"offset"=>request['offset'],"limit"=>request['limit']})			
 	end	
 	
-	def getResponse(request={})
+	def getHookResponses(request={})
 		request["offset"].to_s.empty? ? request['offset'] = 0 : request["offset"]
 		request["limit"].to_s.empty? ? request['limit'] = 10 : request["limit"]
-		return request("GET","/hook/" + (request['hook_id']).to_s + "/response",{"offset"=>request['offset'],"limit"=>request['limit']})			
+		return request("GET","/hooks/" + (request['hook_id']).to_s + "/responses",{"offset"=>request['offset'],"limit"=>request['limit']})			
 	end
 end
